3a81f23a313647754ec0a03cea71516aef9df011
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-09 16:31:46 +0200

    + test multiple

--- test/MR.AttributeDI.Abstractions.Tests/CollectorTest.cs
+++ test/MR.AttributeDI.Abstractions.Tests/CollectorTest.cs
@@ -7,7 +7,17 @@ using Xunit;
 namespace MR.AttributeDI
 {
 	[AddToServices]
-	public class Service
+	public class Service1
+	{
+	}
+
+	public interface IService2
+	{
+	}
+
+	[AddToServices]
+	[AddToServices(As = typeof(IService2))]
+	public class Service2 : IService2
 	{
 	}
 
@@ -57,7 +67,19 @@ namespace MR.AttributeDI
 			var collector = Create();
 			collector.Collect(applier);
 
-			applier.Contexts.Should().Contain((c) => c.Service == typeof(Service));
+			applier.Contexts.Should().Contain((c) => c.Service == typeof(Service1));
+		}
+
+		[Fact]
+		public void Collect_Multiple()
+		{
+			var applier = new FakeApplier();
+			var collector = Create();
+			collector.Collect(applier);
+
+			applier.Contexts.Should().Contain((c) =>
+				c.Service == typeof(Service2) && c.As == typeof(Service2)).And.Contain((c) =>
+				c.Service == typeof(Service2) && c.As == typeof(IService2));
 		}
 
 		private Collector Create() => new Collector(typeof(CollectorTest).Assembly);
